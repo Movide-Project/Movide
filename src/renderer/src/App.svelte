5ebde1ed6a27f57d2e56cfc6cab169e0b40e1733
Kha1r0 <n.kuckeburg@outlook.de>
2024-11-15 00:08:20 +0100

    add: MOTIS logo

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import Versions from './components/Versions.svelte'
-  import electronLogo from './assets/electron.svg'
+  import logo from './assets/logo.svg'
 
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 </script>
 
-<img alt="logo" class="logo" src={electronLogo} />
+<img alt="logo" class="logo" src={logo} />
+
 <div class="creator">Powered by electron-vite</div>
 <div class="text">
   Build an Electron app with
